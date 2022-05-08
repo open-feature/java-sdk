3ba3f2c4a4d2c7435a113037425e557d83565218
Justin Abrahms <justin@abrah.ms>
2022-05-08 15:39:26 -0700

    Don't have to implement each method on a hook, just the ones we care about.

--- lib/src/main/java/javasdk/Hook.java
+++ lib/src/main/java/javasdk/Hook.java
@@ -1,9 +1,9 @@
 package javasdk;
 
 // TODO: interface? or abstract class?
-public interface Hook<T> {
-    void before(HookContext<T> ctx);
-    void after(HookContext<T> ctx, FlagEvaluationDetails<T> details);
-    void error(HookContext<T> ctx, Exception error);
-    void afterAll(HookContext<T> ctx);
+public abstract class Hook<T> {
+    void before(HookContext<T> ctx) {}
+    void after(HookContext<T> ctx, FlagEvaluationDetails<T> details) {}
+    void error(HookContext<T> ctx, Exception error) {}
+    void afterAll(HookContext<T> ctx) {}
 }
