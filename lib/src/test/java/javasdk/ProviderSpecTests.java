365491b918f4a0cb9be7c65eab56a98af4ff55e5
Justin Abrahms <justin@abrah.ms>
2022-05-11 16:07:44 -0700

    Delete duped specs.

--- lib/src/test/java/javasdk/ProviderSpecTests.java
+++ lib/src/test/java/javasdk/ProviderSpecTests.java
@@ -85,16 +85,4 @@ public class ProviderSpecTests {
             "flag value resolution.")
     @Disabled("I don't think we should do that until we figure out the call signature differences")
     @Test void not_doing() {}
-    @Specification(spec="provider", number="2.1", text="The provider interface MUST define a name field or accessor, which identifies the provider implementation.")
-    @Specification(spec="provider", number="2.2", text="The feature provider interface MUST define methods to resolve flag values, with parameters flag key (string, required), default value (boolean | number | string | structure, required), evaluation context (optional), and evaluation options (optional), which returns a flag resolution structure.")
-    @Specification(spec="provider", number="2.3.1", text="The feature provider interface MUST define methods for typed flag resolution, including boolean, numeric, string, and structure.")
-    @Specification(spec="provider", number="2.4", text="In cases of normal execution, the provider MUST populate the flag resolution structure's value field with the resolved flag value.")
-    @Specification(spec="provider", number="2.5", text="In cases of normal execution, the provider SHOULD populate the flag resolution structure's variant field with a string identifier corresponding to the returned flag value.")
-    @Specification(spec="provider", number="2.6", text="The provider SHOULD populate the flag resolution structure's reason field with a string indicating the semantic reason for the returned flag value.")
-    @Specification(spec="provider", number="2.7", text="In cases of normal execution, the provider MUST NOT populate the flag resolution structure's error code field, or otherwise must populate it with a null or falsy value.")
-    @Specification(spec="provider", number="2.8", text="In cases of abnormal execution, the provider MUST indicate an error using the idioms of the implementation language, with an associated error code having possible values PROVIDER_NOT_READY, FLAG_NOT_FOUND, PARSE_ERROR, TYPE_MISMATCH, or GENERAL.")
-    @Specification(spec="provider", number="2.9.1", text="The flag resolution structure SHOULD accept a generic argument (or use an equivalent language feature) which indicates the type of the wrapped value field.")
-    @Specification(spec="provider", number="2.10", text="The provider interface MAY define a context transformer method or function, which can be optionally implemented in order to transform the evaluation context prior to flag value resolution.")
-    @Specification(spec="provider", number="2.11.1", text="If the implementation includes a context transformer, the provider SHOULD accept a generic argument (or use an equivalent language feature) indicating the type of the transformed context. If such type information is supplied, more accurate type information can be supplied in the flag resolution methods.")
-    @Disabled @Test void todo() {}
 }
