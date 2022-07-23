cfc94c5b77334b3471b177efa8f9dac1cecd41ad
Thomas Poignant <thomas.poignant@gofeatureflag.org>
2022-07-23 19:35:56 +0200

    Add method to get double
    Signed-off-by: Thomas Poignant <thomas.poignant@gofeatureflag.org>
    Signed-off-by: Robert Grassian <robert.grassian@split.io>

--- src/main/java/dev/openfeature/javasdk/FeatureProvider.java
+++ src/main/java/dev/openfeature/javasdk/FeatureProvider.java
@@ -8,5 +8,6 @@ public interface FeatureProvider {
     ProviderEvaluation<Boolean> getBooleanEvaluation(String key, Boolean defaultValue, EvaluationContext ctx, FlagEvaluationOptions options);
     ProviderEvaluation<String> getStringEvaluation(String key, String defaultValue, EvaluationContext ctx, FlagEvaluationOptions options);
     ProviderEvaluation<Integer> getIntegerEvaluation(String key, Integer defaultValue, EvaluationContext ctx, FlagEvaluationOptions options);
+    ProviderEvaluation<Double> getDoubleEvaluation(String key, Double defaultValue, EvaluationContext ctx, FlagEvaluationOptions options);
     <T> ProviderEvaluation<T> getObjectEvaluation(String key, T defaultValue, EvaluationContext invocationContext, FlagEvaluationOptions options);
 }
--- src/test/java/dev/openfeature/javasdk/AlwaysBrokenProvider.java
+++ src/test/java/dev/openfeature/javasdk/AlwaysBrokenProvider.java
@@ -28,6 +28,11 @@ public class AlwaysBrokenProvider implements FeatureProvider {
     }
 
     @Override
+    public ProviderEvaluation<Double> getDoubleEvaluation(String key, Double defaultValue, EvaluationContext ctx, FlagEvaluationOptions options) {
+        throw new NotImplementedException("BORK");
+    }
+
+    @Override
     public <T> ProviderEvaluation<T> getObjectEvaluation(String key, T defaultValue, EvaluationContext invocationContext, FlagEvaluationOptions options) {
         throw new NotImplementedException("BORK");
     }
