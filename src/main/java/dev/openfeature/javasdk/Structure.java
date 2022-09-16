d8b4d0de64a0ed7d6687b91019463e26564af8be
Thomas Poignant <thomas.poignant@gofeatureflag.org>
2022-09-16 15:54:16 +0200

    Add asObjectMap to get the EvaluationContext as Map<String,Object>
    Signed-off-by: Thomas Poignant <thomas.poignant@gofeatureflag.org>

--- src/main/java/dev/openfeature/javasdk/Structure.java
+++ src/main/java/dev/openfeature/javasdk/Structure.java
@@ -1,11 +1,10 @@
 package dev.openfeature.javasdk;
 
 import java.time.Instant;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
+import dev.openfeature.javasdk.exceptions.ValueNotConvertableError;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
 
@@ -66,9 +65,9 @@ public class Structure {
         return this;
     }
 
-    /** 
+    /**
      * Add date-time relevant key.
-     * 
+     *
      * @param key feature key
      * @param value date-time value
      * @return Structure
@@ -90,10 +89,73 @@ public class Structure {
 
     /**
      * Get all values.
-     * 
+     *
      * @return all attributes on the structure
      */
     public Map<String, Value> asMap() {
         return new HashMap<>(this.attributes);
+    }
+
+    /**
+     * Get all values, with primitives types.
+     *
+     * @return all attributes on the structure into a Map<String,Object>
+     */
+    public Map<String, Object> asObjectMap() {
+        return attributes
+                .entrySet()
+                .stream()
+                .collect(Collectors.toMap(
+                        Map.Entry::getKey,
+                        e -> convertValue(getValue(e.getKey()))
+                ));
+    }
+
+    /**
+     * convertValue is converting the object type Value in a primitive type.
+     * @param value - Value object to convert
+     * @return an Object containing the primitive type.
+     */
+    private Object convertValue(Value value){
+        if (value.isBoolean()){
+            return value.asBoolean();
+        }
+
+        if(value.isNumber()){
+            Double valueAsDouble = value.asDouble();
+            if ((valueAsDouble == Math.floor(valueAsDouble)) && !Double.isInfinite(valueAsDouble)) {
+                return value.asInteger();
+            }
+            return valueAsDouble;
+        }
+
+        if(value.isString()){
+            return value.asString();
+        }
+
+        if(value.isInstant()) {
+            return value.asInstant();
+        }
+
+        if(value.isList()){
+            return value.asList()
+                    .stream()
+                    .map(this::convertValue)
+                    .collect(Collectors.toList());
+        }
+
+        if(value.isStructure()){
+            Structure s = value.asStructure();
+            return s.asMap()
+                    .keySet()
+                    .stream()
+                    .collect(
+                        Collectors.toMap(
+                            key -> key,
+                            key-> convertValue(s.getValue(key))
+                        )
+                    );
+        }
+        throw new ValueNotConvertableError();
     }
 }
