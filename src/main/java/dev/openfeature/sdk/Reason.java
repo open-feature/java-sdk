d069a8fa9d7c1795f6713f4b331657119e6f7d8f
Todd Baert <toddbaert@gmail.com>
2023-01-19 02:18:52 -0500

    feat: add STATIC, CACHED reasons (#240)
    feat: add STATIC, CACHED reasons.
    
    Signed-off-by: Todd Baert <toddbaert@gmail.com>
    
    Signed-off-by: Todd Baert <toddbaert@gmail.com>

--- src/main/java/dev/openfeature/sdk/Reason.java
+++ src/main/java/dev/openfeature/sdk/Reason.java
@@ -4,5 +4,5 @@ package dev.openfeature.sdk;
  * Predefined resolution reasons.
  */
 public enum Reason {
-    DISABLED, SPLIT, TARGETING_MATCH, DEFAULT, UNKNOWN, ERROR
+    DISABLED, SPLIT, TARGETING_MATCH, DEFAULT, UNKNOWN, CACHED, STATIC, ERROR
 }
