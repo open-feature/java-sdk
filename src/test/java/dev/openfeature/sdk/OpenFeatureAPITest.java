efae4770d5fd9c96f15cb8d7520f6ffb5fb2702e
Justin Abrahms <justin@abrah.ms>
2023-05-06 23:22:04 -0700

    Add a few missing tests
    Signed-off-by: Justin Abrahms <justin@abrah.ms>

--- src/test/java/dev/openfeature/sdk/OpenFeatureAPITest.java
+++ src/test/java/dev/openfeature/sdk/OpenFeatureAPITest.java
@@ -3,6 +3,7 @@ package dev.openfeature.sdk;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class OpenFeatureAPITest {
     @Test
@@ -11,5 +12,15 @@ public class OpenFeatureAPITest {
         FeatureProvider provider = new NoOpProvider();
         api.setProvider("namedProviderTest", provider);
         assertEquals(provider.getMetadata().getName(), api.getProviderMetadata("namedProviderTest").getName());
+    }
+
+    @Test void settingDefaultProviderToNullErrors() {
+        OpenFeatureAPI api = OpenFeatureAPI.getInstance();
+        assertThrows(IllegalArgumentException.class, () -> api.setProvider(null));
+    }
+
+    @Test void settingNamedClientProviderToNullErrors() {
+        OpenFeatureAPI api = OpenFeatureAPI.getInstance();
+        assertThrows(IllegalArgumentException.class, () -> api.setProvider("client-name", null));
     }
 }
