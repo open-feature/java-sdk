693721e36c5b31adacd96afc55bc38ed53534db4
Todd Baert <todd.baert@dynatrace.com>
2023-06-28 10:40:10 -0400

    feat: add empty constructors to data classes (#491)
    Signed-off-by: Todd Baert <todd.baert@dynatrace.com>

--- src/main/java/dev/openfeature/sdk/ProviderEvaluation.java
+++ src/main/java/dev/openfeature/sdk/ProviderEvaluation.java
@@ -1,12 +1,21 @@
 package dev.openfeature.sdk;
 
-import lombok.Builder;
-import lombok.Data;
-
 import javax.annotation.Nullable;
 
-@SuppressWarnings("checkstyle:MissingJavadocType")
-@Data @Builder
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Contains information about how the a flag was evaluated, including the resolved value.
+ *
+ * @param <T> the type of the flag being evaluated.
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProviderEvaluation<T> implements BaseEvaluation<T> {
     T value;
     @Nullable String variant;
