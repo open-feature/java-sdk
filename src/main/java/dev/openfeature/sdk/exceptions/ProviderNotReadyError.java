8f4d50a27f6a89610bc3998d02b66bb4d143fee3
Todd Baert <todd.baert@dynatrace.com>
2023-08-15 16:03:00 -0400

    chore: fix codecov, throw in memory provider
    Signed-off-by: Todd Baert <todd.baert@dynatrace.com>

---
+++ src/main/java/dev/openfeature/sdk/exceptions/ProviderNotReadyError.java
@@ -0,0 +1,12 @@
+package dev.openfeature.sdk.exceptions;
+
+import dev.openfeature.sdk.ErrorCode;
+import lombok.Getter;
+import lombok.experimental.StandardException;
+
+@SuppressWarnings("checkstyle:MissingJavadocType")
+@StandardException
+public class ProviderNotReadyError extends OpenFeatureError {
+    private static final long serialVersionUID = 1L;
+    @Getter private final ErrorCode errorCode = ErrorCode.PROVIDER_NOT_READY;
+}
