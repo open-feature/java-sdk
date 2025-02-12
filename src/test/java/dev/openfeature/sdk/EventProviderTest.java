eb103675c319dc293225cb529b3759a1fe73dda9
Philipp Fehre <philipp@fehre.co.uk>
2025-02-12 15:28:55 +0100

    Don't reuse the JVM Process
    Signed-off-by: Philipp Fehre <philipp@fehre.co.uk>

--- src/test/java/dev/openfeature/sdk/EventProviderTest.java
+++ src/test/java/dev/openfeature/sdk/EventProviderTest.java
@@ -12,7 +12,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Timeout;
-import org.mockito.Mockito;
 
 class EventProviderTest {
 
