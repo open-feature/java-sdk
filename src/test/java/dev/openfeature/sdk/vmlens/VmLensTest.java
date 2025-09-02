0fdedcb2ce006819262edf1c188d6cb3b02327da
christian.lutnik <christian.lutnik@dynatrace.com>
2025-09-02 10:14:12 +0200

    remove unused import
    Signed-off-by: christian.lutnik <christian.lutnik@dynatrace.com>

--- src/test/java/dev/openfeature/sdk/vmlens/VmLensTest.java
+++ src/test/java/dev/openfeature/sdk/vmlens/VmLensTest.java
@@ -1,7 +1,6 @@
 package dev.openfeature.sdk.vmlens;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
