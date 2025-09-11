711e0beeb8ad3aaf655e59338ddef9ad1116dc53
Simon Schrottner <simon.schrottner@dynatrace.com>
2025-09-11 14:41:40 +0200

    fixup: lazy init
    Signed-off-by: Simon Schrottner <simon.schrottner@dynatrace.com>

--- src/main/java/dev/openfeature/sdk/HookData.java
+++ src/main/java/dev/openfeature/sdk/HookData.java
@@ -48,21 +48,27 @@ public interface HookData {
      * Default implementation of HookData.
      */
     public class DefaultHookData implements HookData {
-        private final Map<String, Object> data = new HashMap<>();
+        private Map<String, Object> data;
 
         @Override
         public void set(String key, Object value) {
+            if(data == null) {
+                data = new HashMap<>();
+            }
             data.put(key, value);
         }
 
         @Override
         public Object get(String key) {
+            if(data == null) {
+                return null;
+            }
             return data.get(key);
         }
 
         @Override
         public <T> T get(String key, Class<T> type) {
-            Object value = data.get(key);
+            Object value = get(key);
             if (value == null) {
                 return null;
             }
