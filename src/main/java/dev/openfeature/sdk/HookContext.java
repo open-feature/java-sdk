1b08e3db42635bbe79c61437db2359bd74a98348
alexandraoberaigner <82218944+alexandraoberaigner@users.noreply.github.com>
2025-09-15 14:50:16 +0200

    feat: add hook data  (#1587)
    * Added hook data
    
    Signed-off-by: mdxabu <abdullahfakrudeen2020@gmail.com>
    
    * to support null values while maintaining thread safety, you must avoid ConcurrentHashMap and use a synchronized HashMap instead
    
    Signed-off-by: mdxabu <abdullahfakrudeen2020@gmail.com>
    
    * Build successfully runned:spotless plugin
    
    Signed-off-by: mdxabu <abdullahfakrudeen2020@gmail.com>
    
    * fixup: fix comment
    
    Co-authored-by: alexandraoberaigner <82218944+alexandraoberaigner@users.noreply.github.com>
    Signed-off-by: Simon Schrottner <simon.schrottner@dynatrace.com>
    
    * fixup: remove synchronized map
    
    Co-authored-by: alexandraoberaigner <82218944+alexandraoberaigner@users.noreply.github.com>
    Signed-off-by: Simon Schrottner <simon.schrottner@dynatrace.com>
    
    * fixup: removed unneeded import
    
    Signed-off-by: Simon Schrottner <simon.schrottner@dynatrace.com>
    
    * fixup: spotless
    
    Signed-off-by: Simon Schrottner <simon.schrottner@dynatrace.com>
    
    * Update src/main/java/dev/openfeature/sdk/HookData.java
    
    Co-authored-by: chrfwow <christian.lutnik@dynatrace.com>
    Signed-off-by: Abu <abdullahfakrudeen2020@gmail.com>
    
    * Update src/main/java/dev/openfeature/sdk/HookData.java
    
    Co-authored-by: chrfwow <christian.lutnik@dynatrace.com>
    Signed-off-by: Abu <abdullahfakrudeen2020@gmail.com>
    
    * feat: add support for hook data sharing between stages
    
    Signed-off-by: Alexandra Oberaigner <alexandra.oberaigner@dynatrace.com>
    
    * fixup: tests
    
    Signed-off-by: Simon Schrottner <simon.schrottner@dynatrace.com>
    
    * fixup: pr comments and extraction of own pair class
    
    Signed-off-by: Simon Schrottner <simon.schrottner@dynatrace.com>
    
    * feat: add HookContext decorator
    
    Signed-off-by: Simon Schrottner <simon.schrottner@dynatrace.com>
    
    * fixup: add default method for interface
    
    Signed-off-by: Simon Schrottner <simon.schrottner@dynatrace.com>
    
    * feat: performance boost
    
    Signed-off-by: Simon Schrottner <simon.schrottner@dynatrace.com>
    
    * feat: performance improvements
    
    Signed-off-by: Simon Schrottner <simon.schrottner@dynatrace.com>
    
    * fixup: lazy init
    
    Signed-off-by: Simon Schrottner <simon.schrottner@dynatrace.com>
    
    * fixup: pr comments
    
    Signed-off-by: Simon Schrottner <simon.schrottner@dynatrace.com>
    
    ---------
    
    Signed-off-by: mdxabu <abdullahfakrudeen2020@gmail.com>
    Signed-off-by: Simon Schrottner <simon.schrottner@dynatrace.com>
    Signed-off-by: Abu <abdullahfakrudeen2020@gmail.com>
    Signed-off-by: Alexandra Oberaigner <alexandra.oberaigner@dynatrace.com>
    Co-authored-by: mdxabu <abdullahfakrudeen2020@gmail.com>
    Co-authored-by: Simon Schrottner <simon.schrottner@dynatrace.com>
    Co-authored-by: Michael Beemer <beeme1mr@users.noreply.github.com>
    Co-authored-by: chrfwow <christian.lutnik@dynatrace.com>

--- src/main/java/dev/openfeature/sdk/HookContext.java
+++ src/main/java/dev/openfeature/sdk/HookContext.java
@@ -1,32 +1,13 @@
 package dev.openfeature.sdk;
 
-import lombok.Builder;
-import lombok.NonNull;
-import lombok.Value;
-import lombok.With;
+import dev.openfeature.sdk.HookContextWithoutData.HookContextWithoutDataBuilder;
 
 /**
- * A data class to hold immutable context that {@link Hook} instances use.
- *
- * @param <T> the type for the flag being evaluated
+ * A interface to hold immutable context that {@link Hook} instances use.
  */
-@Value
-@Builder
-@With
-public class HookContext<T> {
-    @NonNull String flagKey;
-
-    @NonNull FlagValueType type;
-
-    @NonNull T defaultValue;
-
-    @NonNull EvaluationContext ctx;
-
-    ClientMetadata clientMetadata;
-    Metadata providerMetadata;
-
+public interface HookContext<T> {
     /**
-     * Builds a {@link HookContext} instances from request data.
+     * Builds a {@link HookContextWithoutData} instances from request data.
      *
      * @param key              feature flag key
      * @param type             flag value type
@@ -36,21 +17,39 @@ public class HookContext<T> {
      * @param defaultValue     Fallback value
      * @param <T>              type that the flag is evaluating against
      * @return resulting context for hook
+     * @deprecated this should not be instantiated outside the SDK anymore
      */
-    public static <T> HookContext<T> from(
+    @Deprecated
+    static <T> HookContext<T> from(
             String key,
             FlagValueType type,
             ClientMetadata clientMetadata,
             Metadata providerMetadata,
             EvaluationContext ctx,
             T defaultValue) {
-        return HookContext.<T>builder()
-                .flagKey(key)
-                .type(type)
-                .clientMetadata(clientMetadata)
-                .providerMetadata(providerMetadata)
-                .ctx(ctx)
-                .defaultValue(defaultValue)
-                .build();
+        return new HookContextWithoutData<>(key, type, defaultValue, ctx, clientMetadata, providerMetadata);
+    }
+
+    /**
+     * Returns a builder for our default HookContext object.
+     */
+    static <T> HookContextWithoutDataBuilder<T> builder() {
+        return HookContextWithoutData.<T>builder();
+    }
+
+    String getFlagKey();
+
+    FlagValueType getType();
+
+    T getDefaultValue();
+
+    EvaluationContext getCtx();
+
+    ClientMetadata getClientMetadata();
+
+    Metadata getProviderMetadata();
+
+    default HookData getHookData() {
+        return null;
     }
 }
