b81805764669f8c44d506e09a630e032b48fa095
ThomasKrieger <thomas.krieger@vmlens.com>
2025-09-24 20:07:11 +0200

    added await in ProviderSteps and removed Awaitility.await() since it is not needed anymore since we wait during the setting of the states
    Signed-off-by: ThomasKrieger <thomas.krieger@vmlens.com>

--- src/test/java/dev/openfeature/sdk/e2e/steps/ProviderSteps.java
+++ src/test/java/dev/openfeature/sdk/e2e/steps/ProviderSteps.java
@@ -25,9 +25,7 @@ import dev.openfeature.sdk.providers.memory.Flag;
 import dev.openfeature.sdk.providers.memory.InMemoryProvider;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
-import java.time.Duration;
 import java.util.Map;
-import org.awaitility.Awaitility;
 
 public class ProviderSteps {
     private final State state;
@@ -114,19 +112,15 @@ public class ProviderSteps {
         switch (providerState) {
             case FATAL:
             case ERROR:
-                mockProvider.emitProviderReady(details);
-                mockProvider.emitProviderError(details);
+                mockProvider.emitProviderReady(details).await();
+                mockProvider.emitProviderError(details).await();
                 break;
             case STALE:
-                mockProvider.emitProviderReady(details);
-                mockProvider.emitProviderStale(details);
+                mockProvider.emitProviderReady(details).await();
+                mockProvider.emitProviderStale(details).await();
                 break;
             default:
         }
-        Awaitility.await().atMost(Duration.ofSeconds(30)).until(() -> {
-            ProviderState providerState1 = client.getProviderState();
-            return providerState1 == providerState;
-        });
     }
 
     private void configureMockEvaluations(FeatureProvider mockProvider, ErrorCode errorCode, String errorMessage) {
