f07ae2bf709d43e539dccfb4b6bc9f1e70eb1e7d
jarebudev <23311805+jarebudev@users.noreply.github.com>
2025-11-04 23:06:27 +0000

    tidy up
    Signed-off-by: jarebudev <23311805+jarebudev@users.noreply.github.com>

--- src/main/java/dev/openfeature/sdk/internal/ConfigurableThreadFactory.java
+++ src/main/java/dev/openfeature/sdk/internal/ConfigurableThreadFactory.java
@@ -1,19 +1,16 @@
 package dev.openfeature.sdk.internal;
 
-import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * A configurable thread factory for internal use in the SDK.
- * Allows daemon or non-daemon threads to be created with a custom name prefix,
- * and a delegate ThreadFactory if required.
+ * Allows daemon or non-daemon threads to be created with a custom name prefix.
  */
 public final class ConfigurableThreadFactory implements ThreadFactory {
 
     private final AtomicInteger counter = new AtomicInteger();
     private final String namePrefix;
-    private final ThreadFactory delegate;
     private final boolean daemon;
 
     /**
@@ -22,7 +19,7 @@ public final class ConfigurableThreadFactory implements ThreadFactory {
      * @param namePrefix    Prefix used for setting the new thread's name.
      */
     public ConfigurableThreadFactory(String namePrefix) {
-        this(namePrefix, Executors.defaultThreadFactory(), false);
+        this(namePrefix, false);
     }
 
     /**
@@ -32,25 +29,13 @@ public final class ConfigurableThreadFactory implements ThreadFactory {
      * @param daemon        Whether daemon or non-daemon threads will be created.
      */
     public ConfigurableThreadFactory(String namePrefix, boolean daemon) {
-        this(namePrefix, Executors.defaultThreadFactory(), daemon);
-    }
-
-    /**
-     * {@link ConfigurableThreadFactory}'s constructor.
-     *
-     * @param namePrefix    Prefix used for setting the new thread's name.
-     * @param delegate      Delegate ThreadFactory for creating threads.
-     * @param daemon        Whether daemon or non-daemon threads will be created.
-     */
-    public ConfigurableThreadFactory(String namePrefix, ThreadFactory delegate, boolean daemon) {
         this.namePrefix = namePrefix;
-        this.delegate = delegate;
         this.daemon = daemon;
     }
 
     @Override
     public Thread newThread(Runnable runnable) {
-        final Thread thread = delegate.newThread(runnable);
+        final Thread thread = new Thread(runnable);
         thread.setDaemon(daemon);
         thread.setName(namePrefix + "-" + counter.incrementAndGet());
         return thread;
--- src/test/java/dev/openfeature/sdk/internal/ConfigurableThreadFactoryTest.java
+++ src/test/java/dev/openfeature/sdk/internal/ConfigurableThreadFactoryTest.java
@@ -1,22 +1,13 @@
 package dev.openfeature.sdk.internal;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
-import java.util.concurrent.ThreadFactory;
 import org.junit.jupiter.api.Test;
 
 class ConfigurableThreadFactoryTest {
 
     private static final String THREAD_NAME = "testthread";
-    private final Runnable runnable = new Runnable() {
-        @Override
-        public void run() {}
-    };
+    private final Runnable runnable = () -> {};
 
     @Test
     void verifyNewThreadHasNamePrefix() {
@@ -37,34 +28,6 @@ class ConfigurableThreadFactoryTest {
 
         assertThat(threadOne.getName()).isEqualTo(THREAD_NAME + "-1");
         assertThat(threadTwo.getName()).isEqualTo(THREAD_NAME + "-2");
-    }
-
-    @Test
-    void verifyNewThreadWithDelegateHasNamePrefix() {
-
-        var threadFactoryMock = mock(ThreadFactory.class);
-        var threadMock = mock(Thread.class);
-        var configurableThreadFactory = new ConfigurableThreadFactory(THREAD_NAME, threadFactoryMock, false);
-        when(threadFactoryMock.newThread(eq(runnable))).thenReturn(threadMock);
-
-        var thread = configurableThreadFactory.newThread(runnable);
-
-        verify(threadMock, times(1)).setName(THREAD_NAME + "-1");
-        verify(threadMock, times(1)).setDaemon(false);
-    }
-
-    @Test
-    void verifyNewDaemonThreadWithDelegateHasNamePrefix() {
-
-        var threadFactoryMock = mock(ThreadFactory.class);
-        var threadMock = mock(Thread.class);
-        var configurableThreadFactory = new ConfigurableThreadFactory(THREAD_NAME, threadFactoryMock, true);
-        when(threadFactoryMock.newThread(eq(runnable))).thenReturn(threadMock);
-
-        var thread = configurableThreadFactory.newThread(runnable);
-
-        verify(threadMock, times(1)).setName(THREAD_NAME + "-1");
-        verify(threadMock, times(1)).setDaemon(true);
     }
 
     @Test
