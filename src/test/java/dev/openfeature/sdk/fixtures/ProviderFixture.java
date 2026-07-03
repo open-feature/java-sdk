146561081cd48f21bc0fe261608fffc990ceb97e
Jonathan Norris <jonathan.norris@dynatrace.com>
2026-07-03 10:29:12 -0400

    test: tighten ProviderFixture initialize mock matchers
    Signed-off-by: Jonathan Norris <jonathan.norris@dynatrace.com>

--- src/test/java/dev/openfeature/sdk/fixtures/ProviderFixture.java
+++ src/test/java/dev/openfeature/sdk/fixtures/ProviderFixture.java
@@ -1,13 +1,14 @@
 package dev.openfeature.sdk.fixtures;
 
 import static dev.openfeature.sdk.testutils.stubbing.ConditionStubber.doBlock;
-import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.nullable;
 import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
 
 import dev.openfeature.sdk.FeatureProvider;
+import dev.openfeature.sdk.ImmutableContext;
 import dev.openfeature.sdk.ProviderState;
 import java.io.FileNotFoundException;
 import java.util.concurrent.CountDownLatch;
@@ -32,13 +33,15 @@ public class ProviderFixture {
     public static FeatureProvider createMockedErrorProvider() throws Exception {
         FeatureProvider provider = mock(FeatureProvider.class);
         doReturn(ProviderState.NOT_READY).when(provider).getState();
-        doThrow(FileNotFoundException.class).when(provider).initialize(any(), any());
+        doThrow(FileNotFoundException.class).when(provider).initialize(new ImmutableContext(), nullable(String.class));
         return provider;
     }
 
     public static FeatureProvider createBlockedProvider(CountDownLatch latch, Runnable onAnswer) throws Exception {
         FeatureProvider provider = createMockedProvider();
-        doBlock(latch, createAnswerExecutingCode(onAnswer)).when(provider).initialize(any(), any());
+        doBlock(latch, createAnswerExecutingCode(onAnswer))
+                .when(provider)
+                .initialize(new ImmutableContext(), nullable(String.class));
         doReturn("blockedProvider").when(provider).toString();
         return provider;
     }
@@ -57,7 +60,7 @@ public class ProviderFixture {
                     return null;
                 })
                 .when(provider)
-                .initialize(any(), any());
+                .initialize(new ImmutableContext(), nullable(String.class));
         doReturn("unblockingProvider").when(provider).toString();
         return provider;
     }
